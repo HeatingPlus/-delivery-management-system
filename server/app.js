b11725d9feca339943369335b1be21406a0c63b6
Seungyong Choi <ppyooy336@naver.com>
2022-02-20 13:12:59 +0900

    First setting

---
+++ server/app.js
@@ -0,0 +1,32 @@
+const cookieParser = require("cookie-parser");
+var express = require("express");
+const path = require("path");
+var app = express();
+
+require("dotenv").config({
+  path: path.resolve(
+    process.cwd(),
+    process.env.NODE_ENV === "production"
+      ? ".env"
+      : process.env.NODE_ENV === "development"
+      ? ".env.dev"
+      : process.env.NODE_ENV === "local"
+      ? ".env.local"
+      : " "
+  ),
+});
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const port = process.env.SERVER_PORT;
+app.listen(port, () => {
+  console.log(
+    `Server is listening on ${port} \n NODE_ENV = ${process.env.NODE_ENV}`
+  );
+});
+
+app.get("/", (req, res) => {
+  res.send("Heating");
+});
