0fa87388e4bccaf8041bb10fe605d3c0258a0370
ji-mango <susie000301@naver.com>
2022-03-03 16:33:39 +0900

    Add: updateOrder API

--- server/src/app/Admin/adminDao.js
+++ server/src/app/Admin/adminDao.js
@@ -10,6 +10,17 @@ async function selectOrderList(connection) {
     return orderRows;
 }
 
+async function updateOrderDao(connection, orderInfo) {
+    const updateOrderQuery = `
+    UPDATE Orders
+    SET size = ?, phoneNumber = ?, cost = ?, site = ?, status = ?, address = ?
+    WHERE orderIdx = ?
+    `;
+    const [orderRows] = await connection.query(updateOrderQuery, orderInfo);
+    return orderRows;
+}
+
 module.exports = {
-    selectOrderList
+    selectOrderList,
+    updateOrderDao
 };
